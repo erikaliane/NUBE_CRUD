028619d22f8f5b4a4419d8d50ffb45a27145d56b
ErikaVentura <105476284+erikaliane@users.noreply.github.com>
2023-04-01 16:54:19 -0500

    edit creado

--- controllers/crud.js
+++ controllers/crud.js
@@ -14,3 +14,18 @@ exports.save = (req,res)=>{
         }
     })
 }
+
+exports.update = (req,res)=>{
+    const id_producto = req.body.id_producto;
+    const nombre = req.body.nombre;
+    const precio = req.body.precio;
+    const cantidad = req.body.cantidad;
+
+    conexion.query('UPDATE producto SET ? WHERE id = ? ' , [{id:id_producto, nombre:nombre,precio:precio,cantidad:cantidad}, id_producto] ,(error, results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.redirect('/');
+        }
+    })
+}
--- router.js
+++ router.js
@@ -21,7 +21,19 @@ router.get('/create',(req,res)=>{
     res.render('create');
 })
 
+//RUTA PARA EDITAR REGISTROS 
+router.get('/edit/:id' ,(req,res)=>{
+    const id = req.params.id;
+    conexion.query('SELECT * FROM producto WHERE id=?',[id], (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.render('edit', {producto:results[0]});
+        }
+    })
+})
+
 const crud= require('./controllers/crud');
 router.post('/save', crud.save)
-
+router.post('/update', crud.update)
 module.exports = router
