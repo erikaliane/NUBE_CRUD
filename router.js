6480433c7c48edcad552e0c55c6b49ac3828a36b
ErikaVentura <105476284+erikaliane@users.noreply.github.com>
2023-04-01 17:00:55 -0500

    delete creado

--- router.js
+++ router.js
@@ -33,7 +33,21 @@ router.get('/edit/:id' ,(req,res)=>{
     })
 })
 
+//RUTA PARA ELIMINAR EL REGISTRO
+
+router.get('/delete/:id' ,(req,res)=>{
+    const id = req.params.id;
+    conexion.query('DELETE FROM producto WHERE id = ?' , [id], (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.redirect('/');
+        }
+    })
+})
+
 const crud= require('./controllers/crud');
 router.post('/save', crud.save)
 router.post('/update', crud.update)
+
 module.exports = router
