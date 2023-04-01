7f43cd50a4b70648f61dbca097cac5bd85805cb1
ErikaVentura <105476284+erikaliane@users.noreply.github.com>
2023-04-01 12:55:43 -0500

    UPDATE

--- database/db.js
+++ database/db.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 
 
 const conexion = mysql.createConnection({
+    port : process.env.DB_PORT ||"3106",
     host : process.env.DB_HOST || 'localhost',
     user :  process.env.DB_USER || 'root',
     password:  process.env.DB_PASSWORD || '',
